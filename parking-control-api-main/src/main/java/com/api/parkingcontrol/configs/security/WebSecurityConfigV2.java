d66de1b93fb8ec6fdd405112b05e6a8596d7aed6
marciacardoso <marcybr@gmail.com>
2023-11-14 13:16:25 +0100

    Adding FilterChainSecurity

--- parking-control-api-main/src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfigV2.java
+++ parking-control-api-main/src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfigV2.java
@@ -1,26 +1,18 @@
 package com.api.parkingcontrol.configs.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-//@Configuration
-public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
-    @Autowired
-    final UserDetailsServiceImpl userDetailsService;
-
-    public WebSecurityConfig(UserDetailsServiceImpl userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfigV2 {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .httpBasic()
                 .and()
@@ -31,11 +23,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
-    }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+        return http.build();
     }
 
     @Bean
